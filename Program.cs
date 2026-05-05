9d659f34c60be0b79eda928d4413d4c2a69cd8ac
Oriana Aldana <valpoca24@gmail.com>
2026-05-05 19:15:23 -0400

    quitar migrate para restaurar backup

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<MyAMISContext>();
-    db.Database.Migrate();
+    //db.Database.Migrate();
 }
 
 app.UseSwagger();
